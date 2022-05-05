7b133eeb161ac436592c455a76e85b61b4dda979
nanomiguez <zaseznano@gmail.com>
2022-05-05 13:17:26 -0300

    Initial Navigation created

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FC } from 'react';
+
+
+const homePage: FC = () => {
+  return (
+    <View style={styles.container}>
+    <Text>Paso</Text>
+    <StatusBar style="auto" />
+  </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={homePage} />
+
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
